d3b56a327609a05b0d2d24bcc4a65c4cc7c25a5f
Pierre Jodouin <phjodouin@gmail.com>
2016-02-12 20:44:33 -0500

    Boto3 re-write

--- plugins/lookup/get_azs.py
+++ plugins/lookup/get_azs.py
@@ -1,88 +1,97 @@
+# (c) 2016, Pierre Jodouin <pjodouin@virtualcomputing.solutions>
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 from ansible.errors import AnsibleError
 from ansible.plugins.lookup import LookupBase
-import boto.ec2
+
 import os
-import sys
 import time
 import pickle
 
-# region/stack/param
+try:
+    import boto3
+    from botocore.exceptions import ClientError, EndpointConnectionError
+
+    HAS_BOTO3 = True
+except ImportError:
+    HAS_BOTO3 = False
+
+
 class LookupModule(LookupBase):
-  def __init__(self, basedir=None, **kwargs):
-    self.basedir = basedir
-    self.cache_dir = os.path.join(os.environ['HOME'],'.get_azs')
-    self.cache_time = 60
+    """
+    Return a list of availability zones for a region.
+    """
 
-  def check_cache(self, file):
-    now = int(time.time())
-    data = ''
-    if os.path.isfile(file):
-      # check time stamp of file
-      if ( now - int(os.path.getmtime(file)) ) < self.cache_time:
-        fh = open(file, 'r')
-        data = pickle.load(fh)
+    def __init__(self, basedir=None, **kwargs):
+        self.basedir = basedir
+        self.cache_dir = os.path.join(os.environ['HOME'], '.azs_cache')
+        self.cache_time = 10
 
-    return data
+        super(LookupModule, self).__init__(**kwargs)
 
-  def get_regions(self):
-    regions_cache = os.path.join(self.cache_dir, 'regions')
-    regions = self.check_cache(regions_cache)
-    if regions:
-      pass
-    else:
-      try:
-        regions = boto.ec2.regions()
-        regions = [ r.name for r in regions ]
-        fh = open(regions_cache, 'w')
-        pickle.dump(regions, fh)
-      except:
-        raise AnsibleError('Couldn\'t retrieve aws regions')
+    def check_cache(self, file_name):
 
-    return regions
+        now = int(time.time())
+        data = None
 
-  def _get_azs(self, region):
-    az_cache = os.path.join(self.cache_dir, region + '-' + 'azs')
-    azs = self.check_cache(az_cache)
-    if azs:
-      pass
-    else:
-      conn = boto.ec2.connect_to_region(region)
-      azs = [ az.name for az in conn.get_all_zones() ]
-      try:
-        fh = open(az_cache, 'w')
-        pickle.dump(azs, fh)
-      except:
-        azs = ''
+        if os.path.isfile(file_name):
+            if (now - int(os.path.getmtime(file_name))) < self.cache_time:
+                fh = open(file_name, 'r')
+                data = pickle.load(fh)
 
-    return azs
+        return data
 
-  def run(self, terms=None, inject=None, **kwargs):
-    if not os.path.isdir(self.cache_dir):
-      os.mkdir(self.cache_dir)
+    def get_azs(self, region=None):
 
-    regions = self.get_regions()
+        az_cache = os.path.join(self.cache_dir, '{}-azs'.format(region or 'default'))
+        az_names = self.check_cache(az_cache)
 
-    if terms:
-      if terms in regions:
-        region = terms
-      else:
-        raise AnsibleError('%s is not a valid aws region' % terms)
+        if not az_names:
+            try:
+                azs = boto3.client('ec2', region_name=region).describe_availability_zones()['AvailabilityZones']
+            except ClientError, e:
+                raise AnsibleError(e.message)
+            except EndpointConnectionError:
+                raise AnsibleError('Region invalid or unavailable: {}'.format(region))
 
-    else:
-      if 'AWS_REGION' in os.environ:
-        region = os.environ['AWS_REGION']
-        if not region in regions:
-          raise AnsibleError('%s is not a valid aws region' % region)
-      else:
-        raise AnsibleError('aws region not found in argument or AWS_REGION env var')
+            az_names = [az['ZoneName'] for az in azs if az['State'] == 'available']
 
-    azs = []
-    azs = self._get_azs(region)
+            try:
+                fh = open(az_cache, 'w')
+                pickle.dump(az_names, fh)
+            except Exception:
+                az_names = []
 
-    if len(azs) == 0:
-      raise AnsibleError('Nothing was retured by lookup')
+        return az_names
 
-    return azs
+    def run(self, terms=None, inject=None, **kwargs):
+
+        # validate dependencies
+        if not HAS_BOTO3:
+            raise AnsibleError('Boto3 is required for the get_azs plugin.')
+
+        # create cache folder if needed
+        if not os.path.isdir(self.cache_dir):
+            os.mkdir(self.cache_dir)
+
+        region = terms[0] if len(terms) > 0 else None
+
+        return self.get_azs(region)
