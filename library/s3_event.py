ce3cb68d7dc92ac8e82c91c12e20a3f11a6347f2
Pierre Jodouin <phjodouin@gmail.com>
2017-05-06 12:03:39 -0400

    64/140 character limit on function name/ARN

--- library/s3_event.py
+++ library/s3_event.py
@@ -276,12 +276,19 @@ def validate_params(module, aws):
             module.fail_json(
                     msg='Function name {0} is invalid. Names must contain only alphanumeric characters, colons and hyphens.'.format(function_name)
             )
-        if len(function_name) > 64:
-            module.fail_json(msg='Function name "{0}" exceeds 64 character limit'.format(function_name))
+        if ':' in function_name:
+            if len(function_name) > 140:
+                module.fail_json(msg='Function ARN "{0}" exceeds 140 character limit'.format(function_name))
+        else:
+            if len(function_name) > 64:
+                module.fail_json(msg='Function name "{0}" exceeds 64 character limit'.format(function_name))
 
         # check if 'function_name' needs to be expanded in full ARN format
-        if not function_name.startswith('arn:aws:lambda:'):
-            module.params['lambda_function_arn'] = 'arn:aws:lambda:{0}:{1}:function:{2}'.format(aws.region, aws.account_id, function_name)
+        if not module.params['lambda_function_arn'].startswith('arn:aws:lambda:'):
+            function_name = module.params['lambda_function_arn']
+            module.params['lambda_function_arn'] = 'arn:aws:lambda:{0}:{1}:function:{2}'.format(aws.region,
+                                                                                                aws.account_id,
+                                                                                                function_name)
 
     topic_arn = module.params.get('topic_arn')
     if topic_arn:
